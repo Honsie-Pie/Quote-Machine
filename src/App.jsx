eefd46376d5133d51c244f495816ed15cc3ffe62
Miguel Jaramillo <m.jrillo@live.com>
2025-09-01 15:06:03 -0600

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p>{count}</p>
+      <button onClick={() => setCount((prevCount) => prevCount +1)}>Increment</button>
+    </>
+  )
+}
+
+export default App
