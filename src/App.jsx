3f82406c0a1567af3ed2364d4ef975eb1fa0598a
Miguel Jaramillo <m.jrillo@live.com>
2025-09-01 20:59:58 -0600

    Display background image.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Backgrounds from './components/Backgrounds';
 import Filters from './components/Filters';
@@ -11,21 +11,71 @@ const PHASES = {
   FILTERS: "filters"
 };
 
+const BACKGROUNDPROPS = " no-repeat fixed center";
+const BGURL = 'imgs/bgs/';
+const THUMBURL = 'imgs/thumbs/'
+
+const BGLIST = [
+  {
+    url: `url('${BGURL}city.jpg')${BACKGROUNDPROPS}`,
+    thumb: `${THUMBURL}cityT.jpg`
+  },
+  {
+    url: `url('${BGURL}education.jpg')${BACKGROUNDPROPS}`,
+    thumb: `${THUMBURL}educationT.jpg`
+  },
+  {
+    url: `url('${BGURL}film.jpg')${BACKGROUNDPROPS}`,
+    thumb: `${THUMBURL}filmT.jpg`
+  },
+  {
+    url: `url('${BGURL}literature.jpg')${BACKGROUNDPROPS}`,
+    thumb: `${THUMBURL}literatureT.jpg`
+  },
+  {
+    url: `url('${BGURL}love.jpg')${BACKGROUNDPROPS}`,
+    thumb: `${THUMBURL}loveT.jpg`
+  },
+  {
+    url: `url('${BGURL}motivation.jpg')${BACKGROUNDPROPS}`,
+    thumb: `${THUMBURL}motivationT.jpg`
+  },
+  {
+    url: `url('${BGURL}science.jpg')${BACKGROUNDPROPS}`,
+    thumb: `${THUMBURL}scienceT.jpg`
+  },
+  {
+    url: `url('${BGURL}religion.jpg')${BACKGROUNDPROPS}`,
+    thumb: `${THUMBURL}religionT.jpg`
+  },
+  {
+    url: `url('${BGURL}sport.jpg')${BACKGROUNDPROPS}`,
+    thumb: `${THUMBURL}sportT.jpg`
+  },
+  {
+    url: `url('${BGURL}work.jpg')${BACKGROUNDPROPS}`,
+    thumb: `${THUMBURL}workT.jpg`
+  }
+  ];
+
 function App() {
   const [phase, setPhase] = useState(PHASES.QUOTE);
+  const [curBg, setCurBg] = useState(BGLIST[Math.floor(Math.random() * BGLIST.length)].url);
+
+  console.log(curBg)
 
   return (
-    <div>
+    <div className="app">
       {phase === PHASES.QUOTE ?
       <Quotebox />
       : phase === PHASES.LOADING 
       ? <p>loading</p>
       : phase === PHASES.BACKGROUNDS
-      ? <Backgrounds />
+      ? <Backgrounds url={curBg}/>
       : phase === PHASES.FILTERS
       ? <Filters /> : <p>Error</p>}
-      <button onClick={() => setPhase(PHASES.FILTER)}>Filters</button>
-      <button onClick={() => setPhase(PHASES.BACKGROUND)}>Backgrounds</button>
+      <button onClick={() => setPhase(PHASES.FILTERS)}>Filters</button>
+      <button onClick={() => setPhase(PHASES.BACKGROUNDS)}>Backgrounds</button>
       <button onClick={() => setPhase(PHASES.QUOTE)}>Quotes</button>
     </div>
   )
