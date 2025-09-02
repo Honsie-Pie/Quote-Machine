c417a2a6f7e3a8c333fc2a776ad0cf0334c58476
Miguel Jaramillo <m.jrillo@live.com>
2025-09-01 20:09:31 -0600

    Create component files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
 import './App.css'
+import Backgrounds from './components/Backgrounds';
+import Filters from './components/Filters';
+import Quotebox from './components/Quotebox';
+
+const PHASES = {
+  LOADING: "loading",
+  QUOTE: "quote",
+  BACKGROUNDS: "backgrounds",
+  FILTERS: "filters"
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [phase, setPhase] = useState(PHASES.QUOTE);
 
   return (
-    <>
-    <p>{count}</p>
-      <button onClick={() => setCount((prevCount) => prevCount +1)}>Increment</button>
-    </>
+    <div>
+      {phase === PHASES.QUOTE ?
+      <Quotebox />
+      : phase === PHASES.LOADING 
+      ? <p>loading</p>
+      : phase === PHASES.BACKGROUNDS
+      ? <Backgrounds />
+      : phase === PHASES.FILTERS
+      ? <Filters /> : <p>Error</p>}
+      <button onClick={() => setPhase(PHASES.FILTER)}>Filters</button>
+      <button onClick={() => setPhase(PHASES.BACKGROUND)}>Backgrounds</button>
+      <button onClick={() => setPhase(PHASES.QUOTE)}>Quotes</button>
+    </div>
   )
 }
 
---
+++ src/components/Backgrounds.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Backgrounds() {
+  return (
+    <div>
+        <p>This screen will allow the user to change background image.</p>
+    </div>
+  )
+}
