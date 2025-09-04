f861f4020a092e22a0ecb3dfccb9809024d3798f
Miguel Jaramillo <m.jrillo@live.com>
2025-09-03 22:39:40 -0600

    Fetch random quote.

--- src/components/Quotebox.jsx
+++ src/components/Quotebox.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 
-export default function Quotebox() {
+const QUOTESURL = "https://api.quotable.io/random";
+
+export default function Quotebox({ tags }) {
+  const [quote, setQuote] = useState({});
+
+  //Fetch quote
+  async function fetchQuote(){
+    try {
+      const response = await fetch(QUOTESURL);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      setQuote({
+        id: result._id,
+        content: result.content,
+        author: result.author,
+        length: result.length
+      });
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
     <div className="box">
-        <p className="quote-content">This window will display a random quote.</p>
-        <p className="quote-author">Anonymous</p>
+        <p className="quote-content">{quote.content}</p>
+        <p className="quote-author">{quote.author}</p>
     </div>
   )
 }
