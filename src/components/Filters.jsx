1a89eed20753087b0b2a2678121ac4879b60d082
Miguel Jaramillo <m.jrillo@live.com>
2025-09-05 23:36:59 -0600

    Turned tag-list into a grid container.

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Filters({ tags, toggleFilter, clearFilters, setPhase, PHASES }) {
   return (
     <div className="box">
-        <div className="tag-select">
-            {tags.map((tag) => <label className="filter" key={tag.id}>
+        <ul className="tag-select">
+            {tags.map((tag) => <li key={tag.id}><label className="filter-label">
                 <input type="checkbox" checked={tag.active} onChange={() => toggleFilter(tag.id)}/>
                 {tag.name}
-                </label>)}
-        </div>
+                </label></li>)}
+        </ul>
         <div className="controls">
             <button onClick={() => clearFilters()}><i className="fa-solid fa-trash"></i></button>
             <button onClick={() => setPhase(PHASES.QUOTE)}>Apply</button>
