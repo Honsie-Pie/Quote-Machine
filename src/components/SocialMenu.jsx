fac0578462d2b8411de7020b86c746a26ecae61e
Miguel Jaramillo <m.jrillo@live.com>
2025-09-17 17:08:16 -0600

    Create SocialMenu element.

---
+++ src/components/SocialMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SocialMenu() {
+  return (
+    <div className="social-menu">
+        <ul>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Take a snap</li>
+            <li>Copy</li>
+        </ul>
+    </div>
+  )
+}
