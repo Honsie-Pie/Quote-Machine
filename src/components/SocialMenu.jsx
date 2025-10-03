87b93e22de098ee13be405f55ae1f18f71fd0898
Miguel Jaramillo <m.jrillo@live.com>
2025-10-02 22:34:44 -0600

    Add share buttons

--- src/components/SocialMenu.jsx
+++ src/components/SocialMenu.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
-export default function SocialMenu() {
+export default function SocialMenu({ quote }) {
+  const [hide, setHide] = useState(true);
+
+    //Copy quote to clipboard
+    function handleCopy(){
+      navigator.clipboard.writeText(`"${quote.content}."\n-${quote.author}`);
+      setHide(true);
+    }
+
   return (
     <div className="social-menu">
-        <ul>
-            <li>Facebook</li>
-            <li>Twitter</li>
-            <li>Take a snap</li>
-            <li>Copy</li>
-        </ul>
+            <button onClick={() => setHide(h => !h)}><i className="fa-solid fa-share"></i> Share</button>
+            {hide ?
+            <ul></ul>
+            :
+            <ul className="social-list">
+            <li><button onClick={() => setHide(true)}><a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank"><i class="fa-brands fa-facebook"></i> Share</a></button></li>
+            <li><button onClick={() => setHide(true)}><a href='https://twitter.com/intent/tweet?text=' target='_blank'><i class="fa-brands fa-x-twitter"></i> Tweet</a></button></li>
+            <li><button onClick={() => handleCopy()}><i className="fa-solid fa-copy"></i> Copy</button></li>
+            </ul>
+            }
     </div>
   )
 }
