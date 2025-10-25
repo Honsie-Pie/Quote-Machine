f8b1078312cbc7ae5c4d122ff82bcac70f9e6aa9
Miguel Jaramillo <m.jrillo@live.com>
2025-10-25 13:04:51 -0600

    Unify BgThumb and BgImage into a single element.

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export default function BackgroundImage({ curBg }) {
   return (
     <picture>
---
+++ src/components/Image.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Image({ image, type, toggleFn }) {
+  return (
+    <picture className={type==='thumb' ? 'bg-thumb' : ''} key={image.thumb} onClick={()=>toggleFn(image)}>
+      <source media="screen and (min-width:768px)" srcSet={type==='thumb' ? `${image.thumb}` : `${image.url}`} />
+      <source media="screen and (min-width:200px)" srcSet={type==='thumb' ? `${image.thumbTall}` : `${image.urlTall}`} />
+      <img className="bgImg"
+      src={type==="thumb" ?`${image.urlTall}` : `${image.thumbTall}`}
+      />
+    </picture>
+  )
+}
